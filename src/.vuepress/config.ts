af85b7afce2b03c5d052cd74a9f9433d27d08ea6
yunhao <pokertian0930@163.com>
2024-03-08 16:40:51 +0800

    加个图片

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -5,8 +5,8 @@ export default defineUserConfig({
   base: "/",
 
   lang: "zh-CN",
-  title: "博客演示",
-  description: "vuepress-theme-hope 的博客演示",
+  title: "博客系统-dlut",
+  description: "大连理工大学校内实训博客系统项目",
 
   theme,
 
